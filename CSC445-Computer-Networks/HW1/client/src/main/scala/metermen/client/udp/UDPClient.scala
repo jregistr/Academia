f39ddca053c75049164ed7c779d7f73a9f9687e5
Jeff Registre <jregistr@oswego.edu>
2016-03-03 18:01:44 -0500

    Added udp client

--- CSC445-Computer-Networks/HW1/client/src/main/scala/metermen/client/udp/UDPClient.scala
+++ CSC445-Computer-Networks/HW1/client/src/main/scala/metermen/client/udp/UDPClient.scala
@@ -2,24 +2,27 @@ package metermen.client.udp
 
 import java.net.{DatagramPacket, DatagramSocket, InetAddress}
 
+import com.jeff.dsl.util.Util._
 import metermen.constants.Constants.MAX_UDP_PACKET_SIZE
 
 import scala.collection.mutable.ListBuffer
-import com.jeff.dsl.util.Util._
 
 
-abstract class UDPClient(size: Int, uri: String, port: Int, val name: String) {
+abstract class UDPClient(size: Int, localPort: Int, destAddress: String, destPort: Int, val name: String) {
 
-  private val iNetAddress = InetAddress.getByName(uri)
+  final val TEST_COUNT = 10000
+  final val TIME_OUT = 5000
+
   private val loopCount = Math.ceil(size / MAX_UDP_PACKET_SIZE).toInt
   private val packetSize = (size.toDouble / loopCount).toInt
 
-  protected val socket = new DatagramSocket(port, iNetAddress)
+  protected val socket = new DatagramSocket(localPort, InetAddress.getByName("localhost"))
 
   protected val writePackets = {
     val buffer = new ListBuffer[DatagramPacket]
+    val endInetAddress = InetAddress.getByName(destAddress)
     loop(loopCount, () => {
-      buffer += new DatagramPacket(new Array[Byte](packetSize), 0, packetSize, iNetAddress, port)
+      buffer += new DatagramPacket(new Array[Byte](packetSize), 0, packetSize, endInetAddress, destPort)
     })
     buffer.toList
   }
@@ -32,7 +35,7 @@ abstract class UDPClient(size: Int, uri: String, port: Int, val name: String) {
     buffer.toList
   }
 
-  def process():(String, List[Double])
+  def process(): (String, List[Double])
 
 }
 
--- CSC445-Computer-Networks/HW1/client/src/main/scala/metermen/client/udp/UDPClientRTT.scala
+++ CSC445-Computer-Networks/HW1/client/src/main/scala/metermen/client/udp/UDPClientRTT.scala
@@ -1,5 +1,9 @@
 package metermen.client.udp
 
+import java.net.SocketTimeoutException
+
+import metermen.constants.Constants.NANOS_TO_MILIS
+
 import scala.collection.mutable.ListBuffer
 
 /**
@@ -9,6 +13,25 @@ class UDPClientRTT(size: Int, uri: String, port: Int, name: String) extends UDPC
 
   override def process(): (String, List[Double]) = {
     val buffer = new ListBuffer[Double]
+
+    var i = 0
+    while (i < TEST_COUNT) {
+
+      val b4 = System.nanoTime()
+      writePackets.foreach(packet => {
+        socket.send(packet)
+      })
+
+      socket.setSoTimeout(TIME_OUT)
+      try {
+        readPackets.foreach(packet => socket.receive(packet))
+        buffer += (System.nanoTime() - b4) * NANOS_TO_MILIS
+        i += 1
+      } catch {
+        case s: SocketTimeoutException =>
+        case e: Exception => throw e
+      }
+    }
 
     (name, buffer.toList)
   }
