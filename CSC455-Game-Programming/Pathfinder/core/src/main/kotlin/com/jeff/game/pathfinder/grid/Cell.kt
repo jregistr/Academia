45e97c485360f67ea93836cdf417afd8e9ac0ad3
Jeff Registre <jregistr@oswego.edu>
2016-03-08 15:09:23 -0500

    Commit

--- CSC455-Game-Programming/Pathfinder/core/src/main/kotlin/com/jeff/game/pathfinder/grid/Cell.kt
+++ CSC455-Game-Programming/Pathfinder/core/src/main/kotlin/com/jeff/game/pathfinder/grid/Cell.kt
@@ -1,16 +1,25 @@
 package com.jeff.game.pathfinder.grid
 
+import com.jeff.game.pathfinder.util.option.*
+import java.util.ArrayList
 
-data class Cell(val cost: Int = -1) : Comparable<Cell> {
+data class Cell(val x: Int, val y: Int, val cost: Int = -1, val teleTo: Option<Cell>) {
 
-    override operator fun compareTo(other: Cell): Int {
-        if (cost < other.cost) {
-            return -1
-        } else if (cost == other.cost) {
-            return 0
-        } else {
-            return 1
+    fun neighbors(maxX: Int, maxY: Int): List<Pair<Int, Int>> {
+        val buffer = ArrayList<Pair<Int, Int>>()
+        if (x + 1 < maxX) {
+            buffer.add(Pair(x + 1, y))
         }
+        if (x - 1 >= 0) {
+            buffer.add(Pair(x - 1, y))
+        }
+        if (y + 1 < maxY) {
+            buffer.add(Pair(x, y + 1))
+        }
+        if (y - 1 >= 0) {
+            buffer.add(Pair(x, y - 1))
+        }
+        return buffer
     }
 
 }
---
+++ CSC455-Game-Programming/Pathfinder/core/src/main/kotlin/com/jeff/game/pathfinder/util/option/Option.kt
@@ -0,0 +1,35 @@
+package com.jeff.game.pathfinder.util.option
+
+/**
+ * My bad attempt at copying scala option type.
+ */
+abstract class Option<T>() {
+
+    protected var value: T? = null
+
+    protected  constructor(value: T) : this() {
+        this.value = value
+    }
+
+    abstract fun defined(): Boolean
+    abstract fun get(): T
+
+}
+
+/**
+ * Specialization of an Option with a value
+ */
+final class Some<T>(value: T) : Option<T>(value) {
+    override fun defined(): Boolean = true
+    override fun get(): T = value!!
+}
+
+/**
+ * Specialization of an Option without a value.
+ * @throws NullPointerException if get() is called.
+ */
+final class None<T>() : Option<T>() {
+    override fun defined(): Boolean = false
+    override fun get(): T = throw NullPointerException()
+}
+
