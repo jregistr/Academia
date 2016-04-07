1c746dfc000e5374c85f680fc0b5b2962468dd28
Jeff Registre <jregistr@oswego.edu>
2016-04-07 19:22:46 -0400

    Adding the working project.

---
+++ CSC455-Game-Programming/SuperGalaxy/src/ships/SuperGalaxyDestroyer.java
@@ -0,0 +1,17 @@
+package ships;
+
+import asteroidsfw.ai.Perceptions;
+import asteroidsfw.ai.ShipControl;
+import asteroidsfw.ai.ShipMind;
+
+public class SuperGalaxyDestroyer implements ShipMind {
+    @Override
+    public void init(ShipControl shipControl) {
+
+    }
+
+    @Override
+    public void think(Perceptions perceptions, double v) {
+
+    }
+}
